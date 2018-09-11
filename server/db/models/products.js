0a0d4e88f7c31165179e9215ea04edd6a4bf966e
Calvin Boetto <cmitchellboetto@gmail.com>
2018-09-11 14:16:48 -0500

    merged products model

--- server/db/models/products.js
+++ server/db/models/products.js
@@ -5,22 +5,39 @@ const db = require('../db')
 const Products = db.define('products', {
   title: {
     type: Sequelize.STRING,
-    validation: {
+    allowNull: false,
+    validate: {
       notEmpty: true,
     },
   },
   description: {
     type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: false
+    }
   },
   price: {
     type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   imageUrl: {
     type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   quantity: {
     type: Sequelize.INTEGER,
-    defaultValue: 0
+    defaultValue: 0,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   }
 })
 
