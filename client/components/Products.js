c1fafe42eb4e517acd08587b04d80fdd3ce98589
Calvin Boetto <cmitchellboetto@gmail.com>
2018-09-11 14:20:37 -0500

    visitor products WIP

---
+++ client/components/Products.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { getProducts } from '../reducer'
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state) => {
+  return {
+    products: state.products.products,
+    isFetching: state.products.isFetching
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getProducts: () => dispatch(getProducts())
+  }
+}
+
+class Products extends React.Component {
+
+  componentDidMount(){
+    this.props.getProducts();
+  }
+
+  render() {
+    if (this.props.isFetching === false){
+      return (
+        <div className="products">
+          <h1>Products</h1>
+          <ul id="productsul">
+            {this.props.products.map(product => {
+              return (
+                <li key={product.id}>
+                  <Link to={`./products/${product.id}`}>
+                  <img src={product.imageUrl} />
+                  </Link>
+                  <h3>{product.title}</h3>
+                  <p>{product.price}</p>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    }
+  }
+}
