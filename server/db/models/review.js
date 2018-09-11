04844a311a050c02ba4348405b9aa40e785b3ea8
codaboba <iamphanle@gmail.com>
2018-09-11 12:01:19 -0500

    completed review model w/ test specs

---
+++ server/db/models/review.js
@@ -0,0 +1,36 @@
+const db = require('../db')
+const Sequelize = require('sequelize')
+
+module.exports = db.define('Review', {
+  userId: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  productId: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  comments: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  rating: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      min: 1,
+      max: 5
+    }
+  }
+})
