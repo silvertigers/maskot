3f48ee0334d47a05f8b2ca82486ad7bd04b1611c
Minkyu Yang <parigu2@msn.com>
2018-09-11 23:16:34 -0500

    merge applied on api/products

--- server/api/products.js
+++ server/api/products.js
@@ -1,7 +1,38 @@
 const router = require('express').Router()
-<<<<<<< HEAD
-const {Products} = require('../db/models')
+const Products = require('../db/models/products')
+const Reviews = require('../db/models/reviews')
+const User = require('../db/models/user')
+
 module.exports = router
+
+// GET all products at /api/products
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Products.findAll()
+    res.status(200).json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Products.findById(req.params.productId, {
+      include: [
+        {
+          model: Reviews,
+          where: {
+            productId: req.params.productId
+          },
+          include: [{model: User}]
+        }
+      ]
+    })
+    res.json(product)
+  } catch (err) {
+    next(err)
+  }
+})
 
 router.post('/', async (req, res, next) => {
   try {
@@ -20,9 +51,9 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-router.put('/:id', async (req, res, next) => {
+router.put('/:productId', async (req, res, next) => {
   try {
-    const id = req.params.id;
+    const id = req.params.productId;
 
     const title = req.body.title;
     const description = req.body.description;
@@ -40,28 +71,14 @@ router.put('/:id', async (req, res, next) => {
 
     const editProduct = await Products.findById(id);
     res.json(editProduct)
-=======
-const Products = require('../db/models/products')
-const Reviews = require('../db/models/reviews')
-const User = require('../db/models/user')
-
-module.exports = router
-
-// GET all products at /api/products
-router.get('/', async (req, res, next) => {
-  try {
-    const products = await Products.findAll()
-    res.status(200).json(products)
->>>>>>> master
   } catch (err) {
     next(err)
   }
 })
 
-<<<<<<< HEAD
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:productId', async (req, res, next) => {
   try {
-    const id = req.params.id;
+    const id = req.params.productId;
 
     await Products.destroy({
       where: {
@@ -72,23 +89,8 @@ router.delete('/:id', async (req, res, next) => {
     const removedProduct = await Products.findAll()
 
     res.json(removedProduct)
-=======
-router.get('/:productId', async (req, res, next) => {
-  try {
-    const product = await Products.findById(req.params.productId, {
-      include: [
-        {
-          model: Reviews,
-          where: {
-            productId: req.params.productId
-          },
-          include: [{model: User}]
-        }
-      ]
-    })
-    res.json(product)
->>>>>>> master
   } catch (err) {
     next(err)
   }
 })
+
--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -2,27 +2,82 @@
 
 const {expect} = require('chai')
 const request = require('supertest')
-<<<<<<< HEAD
 
-const db = require('../db')
-const app = require('../index')
-const Products = db.model('products')
-
-describe('Products routes', () => {
-=======
 const db = require('../db')
 const app = require('../index')
 const Products = db.model('products')
 const Reviews = db.model('reviews')
 const User = db.model('user')
 
+describe('Products routes', () => {
+
 describe('GET /api/products', () => {
->>>>>>> master
   beforeEach(() => {
     return db.sync({force: true})
   })
 
-<<<<<<< HEAD
+  after(() => {
+    return Products.truncate({cascade: true})
+  })
+
+  it('should return products', async () => {
+      const descriptionProduct = 'Ultra kawaii super cute mask'
+      await Products.create({
+        name: 'Mask01',
+        imageUrl: 'Mask.png',
+        description: descriptionProduct,
+        price: 10
+      })
+      const res = await request(app)
+        .get('/api/products')
+        .expect(200)
+
+      expect(res.body).to.be.an('array')
+      expect(res.body[0].description).to.be.equal('Ultra kawaii super cute mask')
+    })
+  })
+
+  describe('GET api/products/:productId', () => {
+    beforeEach(() => {
+      return db.sync({force: true})
+    })
+
+    after(() => {
+      return Products.truncate({cascade: true})
+    })
+
+    it('should return a product with the matching productId and reviews for only that product', async () => {
+      const userId = 1
+      const title = 'My review'
+      const comments = `It's super awesome, and I love it!!!!!!`
+      const rating = 5
+
+      const descriptionProduct = 'Ultra kawaii super cute mask'
+      const product = await Products.create({
+        name: 'Mask01',
+        imageUrl: 'Mask.png',
+        description: descriptionProduct,
+        price: 10
+      })
+      await User.create({email: 'cody@email.com'})
+      await Reviews.create({
+        productId: 1,
+        userId,
+        title,
+        comments,
+        rating
+      })
+
+    const res = await request(app)
+      .get(`/api/products/${product.id}`)
+      .expect(200)
+    expect(res.body.reviews).to.exist
+    expect(res.body.reviews[0].title).to.be.equal(title)
+    console.log('res.body', res.body.reviews[0].user)
+    expect(res.body.reviews[0].user.email).to.be.equal('cody@email.com')
+    })
+  })
+
   describe('/api/products/', () => {
 
     it('Create /api/products', async () => {
@@ -41,7 +96,6 @@ describe('GET /api/products', () => {
       expect(res.body.description).to.equal("you should buy this")
     })
   })
-})
 
   describe('/api/products/:id', () => {
 
@@ -92,66 +146,6 @@ describe('GET /api/products', () => {
       expect(res.body.filter(product => {
         return product.id === removedProduct.id
       }).length).to.equal(0)
-=======
-  after(() => {
-    return Products.truncate({cascade: true})
-  })
-
-  it('should return products', async () => {
-    const descriptionProduct = 'Ultra kawaii super cute mask'
-    await Products.create({
-      name: 'Mask01',
-      imageUrl: 'Mask.png',
-      description: descriptionProduct,
-      price: 10
     })
-    const res = await request(app)
-      .get('/api/products')
-      .expect(200)
-
-    expect(res.body).to.be.an('array')
-    expect(res.body[0].description).to.be.equal('Ultra kawaii super cute mask')
-  })
-})
-
-describe('GET api/products/:productId', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
-  })
-
-  after(() => {
-    return Products.truncate({cascade: true})
-  })
-
-  it('should return a product with the matching productId and reviews for only that product', async () => {
-    const userId = 1
-    const title = 'My review'
-    const comments = `It's super awesome, and I love it!!!!!!`
-    const rating = 5
-
-    const descriptionProduct = 'Ultra kawaii super cute mask'
-    const product = await Products.create({
-      name: 'Mask01',
-      imageUrl: 'Mask.png',
-      description: descriptionProduct,
-      price: 10
-    })
-    await User.create({email: 'cody@email.com'})
-    await Reviews.create({
-      productId: 1,
-      userId,
-      title,
-      comments,
-      rating
-    })
-
-    const res = await request(app)
-      .get(`/api/products/${product.id}`)
-      .expect(200)
-    expect(res.body.reviews).to.exist
-    expect(res.body.reviews[0].title).to.be.equal(title)
-    console.log('res.body', res.body.reviews[0].user)
-    expect(res.body.reviews[0].user.email).to.be.equal('cody@email.com')
->>>>>>> master
   })
 })
