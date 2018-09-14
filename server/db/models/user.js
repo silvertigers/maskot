5b4ee1cdbb35030482aeda1a915a94ca828882a1
codaboba <iamphanle@gmail.com>
2018-09-13 22:41:10 -0500

    merged isAdmin column addition to user model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -27,6 +27,10 @@ const User = db.define('user', {
   googleId: {
     type: Sequelize.STRING
   },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
   facebookId: {
     type: Sequelize.STRING
   }
