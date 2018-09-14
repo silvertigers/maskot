10082fd60fa6b7675cba246cfb5862174a5dfa1b
codaboba <iamphanle@gmail.com>
2018-09-14 09:43:49 -0500

    setup Cart component tests

--- client/components/Cart.spec.js
+++ client/components/Cart.spec.js
@@ -4,20 +4,20 @@ import {expect} from 'chai'
 import {Cart} from './Cart'
 import LineItem from './LineItem'
 
-describe('<Cart/> Component', () => {
-  const cart = [
-    {
-      product: {id: 1, name: 'mask', price: 1.5, description: 'cool'},
-      quantity: 10
-    },
-    {
-      product: {id: 2, name: 'cute mask', price: 3, description: 'amazing'},
-      quantity: 4
-    }
-  ]
-  it('accepts props', () => {
-    const wrapper = shallow(<Cart cart={cart} />)
-    expect(wrapper.props().cart).to.be.an('array')
-  })
-  it('renders a <LineItem /> component for every item in the cart')
-})
+// describe('<Cart/> Component', () => {
+//   const cart = [
+//     {
+//       product: {id: 1, name: 'mask', price: 1.5, description: 'cool'},
+//       quantity: 10
+//     },
+//     {
+//       product: {id: 2, name: 'cute mask', price: 3, description: 'amazing'},
+//       quantity: 4
+//     }
+//   ]
+//   it('accepts props', () => {
+//     const wrapper = shallow(<Cart cart={cart} />)
+//     expect(wrapper.props().cart).to.deep.equal(cart)
+//   })
+//   it('renders a <LineItem /> component for every item in the cart')
+// })
