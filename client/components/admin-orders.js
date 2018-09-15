5cf050bb49a516e4804878ccf8005a0b9f9d037c
Minkyu Yang <parigu2@msn.com>
2018-09-14 22:35:34 -0500

    admin filter the order list by status

--- client/components/admin-orders.js
+++ client/components/admin-orders.js
@@ -3,27 +3,61 @@ import {connect} from 'react-redux'
 import { gotOrder } from '../store/order'
 
 class AdminOrders extends Component {
+  constructor() {
+    super()
+    this.state = {
+      filtered: '',
+    }
+    this.orderFilter = this.orderFilter.bind(this)
+  }
+
   async componentDidMount() {
     this.props.gotOrder();
   }
 
-  render() {
-    const { orders } = this.props.orders
+  orderFilter(event) {
+    const word = event.target.value
+    var filterWord = ""
+    if (word === "placed") {
+      filterWord = "placed"
+    } else if (word === "in process") {
+      filterWord = "in process"
+    } else if (word === "completed") {
+      filterWord = "completed"
+    } else if (word === "cancelled") {
+      filterWord = "cancelled"
+    }
+    this.setState({
+      filtered: filterWord,
+    })
+  }
 
+  render() {
+    var { orders } = this.props.orders
+    console.log(this.state.filtered)
     return (
       <div>
         <div className="admin_Order_List">
           <h2>Order list</h2>
+          <div className="order_list">
+            <button value="all" onClick={this.orderFilter}>All Orders</button>
+            <button value="placed" onClick={this.orderFilter}>In placed</button>
+            <button value="in process" onClick={this.orderFilter}>In process</button>
+            <button value="completed" onClick={this.orderFilter}>Completed</button>
+            <button value="cancelled" onClick={this.orderFilter}>Cancelled</button>
+          </div>
           <ul>
             {
               orders[0] ?
               orders.map(order => {
-                return (
-                  <li key={order.id}>
-                    <h2>{order.user ? order.user.email : order.email}</h2>
-                    <h2>status: {order.status}</h2>
-                  </li>
-                )
+                if (order.status === this.state.filtered || !this.state.filtered) {
+                  return (
+                    <li key={order.id}>
+                      <h2>{order.user ? order.user.email : order.email}</h2>
+                      <h2>status: {order.status}</h2>
+                    </li>
+                  )
+                }
               }) :
               <h2>None of products are available at this time</h2>
             }
