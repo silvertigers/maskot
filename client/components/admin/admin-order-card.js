e6d7ef2102109aaec5c0d21a86255304462d5037
codaboba <iamphanle@gmail.com>
2018-09-18 12:34:29 -0500

    fixed nextStatus button on admin orders page

--- client/components/admin/admin-order-card.js
+++ client/components/admin/admin-order-card.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {editedOrder} from '../../store/order'
+import {Button, List, Segment, Select, Dropdown} from 'semantic-ui-react'
 
 class AdminOrderCard extends React.Component {
   constructor(props) {
@@ -11,7 +12,7 @@ class AdminOrderCard extends React.Component {
     }
     this.changeOrderStatus = this.changeOrderStatus.bind(this)
     this.orderCancelled = this.orderCancelled.bind(this)
-    this.orderStatus = this.orderStatus.bind(this)
+    this.nextStatus = this.nextStatus.bind(this)
   }
 
   async changeOrderStatus(event) {
@@ -32,7 +33,7 @@ class AdminOrderCard extends React.Component {
     await this.props.editOrder(cancelOrder)
   }
 
-  async orderStatus(event) {
+  async nextStatus(event) {
     var statuslist = ['placed', 'processing', 'completed']
     var current = statuslist.indexOf(event.target.value)
     if (current === 2) {
@@ -42,52 +43,68 @@ class AdminOrderCard extends React.Component {
       id: event.target.name,
       status: statuslist[current + 1]
     }
-    this.setState({status: event.target.value})
+    this.setState({status: statuslist[current + 1]})
     await this.props.editOrder(orderStatus)
   }
 
   render() {
     const {id, email, userId, sessionId, status} = this.props.order
     return (
-      <li key={id}>
-        {userId ? (
-          <Link to={`/users/${userId}/orders/${id}`}>
-            <h3>{email}</h3>
-          </Link>
-        ) : (
-          <Link to={`/guests/${sessionId}/orders/${id}`}>
-            <h3>{sessionId}</h3>
-          </Link>
-        )}
+      <div key={id} className="single-user-card">
+        <Segment>
+          <List.Item>
+            <List.Content>
+              <List.Header>
+                {userId ? (
+                  <Link to={`/users/${userId}/orders/${id}`}>
+                    <h3>{email}</h3>
+                  </Link>
+                ) : (
+                  <div className="linkColor">
+                    <Link to={`/guests/${sessionId}/orders/${id}`}>
+                      <h3>{sessionId}</h3>
+                    </Link>
+                  </div>
+                )}
+              </List.Header>
 
-        <h2>status: {status}</h2>
-        <select
-          name={id}
-          onChange={this.changeOrderStatus}
-          value={this.state.status}
-        >
-          <option value="placed">Placed</option>
-          <option value="processing">Processing</option>
-          <option value="completed">Completed</option>
-          <option value="cancelled">Cancelled</option>
-        </select>
-        {!(status === 'completed' || status === 'cancelled') && (
-          <div>
-            {/* will leave to fix bugs
-            <button
-              name={id}
-              value={status}
-              onClick={this.orderStatus}
-              type="button"
-            >
-              Next status
-            </button> */}
-            <button name={id} onClick={this.orderCancelled} type="button">
-              Cancel
-            </button>
-          </div>
-        )}
-      </li>
+              <h2>status: {status}</h2>
+              {/* <Dropdown fluid selection options={[{text: "placed", value: "placed"}, {text:"processing", value: "processing"}, {text: "completed", value: "completed"}, {text: "cancelled", value: "cancelled"}]} defaultValue={status} name={id} onChange={this.changeOrderStatus} /> */}
+              <select
+                name={id}
+                onChange={this.changeOrderStatus}
+                value={this.state.status}
+              >
+                <option value="placed">Placed</option>
+                <option value="processing">Processing</option>
+                <option value="completed">Completed</option>
+                <option value="cancelled">Cancelled</option>
+              </select>
+              {!(status === 'completed' || status === 'cancelled') && (
+                <div>
+                  <Button
+                    content="Cancelled"
+                    color="red"
+                    name={id}
+                    onClick={this.orderCancelled}
+                    icon="cancel"
+                    labelPosition="left"
+                  />
+                  <Button
+                    content="Next"
+                    color="olive"
+                    name={id}
+                    value={status}
+                    onClick={this.nextStatus}
+                    icon="right arrow"
+                    labelPosition="right"
+                  />
+                </div>
+              )}
+            </List.Content>
+          </List.Item>
+        </Segment>
+      </div>
     )
   }
 }
